a6a88dbb032ffeed93543707bc567fab311254f1
erezt.dev <erezt.dev@gmail.com>
2018-04-24 13:16:47 +0300

    map.panTo was the missing peice! Now works

--- src/components/GoogleMapsContainer.vue
+++ src/components/GoogleMapsContainer.vue
@@ -10,60 +10,58 @@
         data() {
             return {
                 theMap: "map-for-" + this.name,
-                location: {
-                    type: String,
-                    default: function () {
-                        return 'canada'
-                    }
-                },
+                location: '',
                 latLong: {
-                    latitude: '',
-                    longitude: ''
+                    latitude: 39.5,
+                    longitude: -72
                 },
             }
         },
         props: {
             name,
-            'latitude': {
-                type: Number,
-                default: function () {
-                    return 39.50
-                }
-            },
-            'longitude': {
-                type: Number,
-                default: function () {
-                    return -98.35
-                }
-            },
-            'zoom': {
-                type: Number,
-                default: function () {
-                    return 4
-                }
-            }
+            // 'latitude': {
+            //     type: Number,
+            //     default: function () {
+            //         return 39.50
+            //     }
+            // },
+            // 'longitude': {
+            //     type: Number,
+            //     default: function () {
+            //         return -98.35
+            //     }
+            // },
+            // 'zoom': {
+            //     type: Number,
+            //     default: function () {
+            //         return 4
+            //     }
+            // }
         },
         methods: {
             showMap() {
                 this.map = new google.maps.Map(document.getElementById(this.theMap), {
-                    center: {lat: this.latitude, lng: this.longitude},
-                    zoom: this.zoom
+                    center: {lat: this.latLong.latitude, lng: this.latLong.longitude},
+                    zoom: 5//this.zoom
                 });
             },
             geoDecoding() {
                 let geocoder = new google.maps.Geocoder();
                 let theLocation = this.location;
                 let latLong = this.latLong;
-                console.log(theLocation.default());
+                //console.log(theLocation.default());
+                self = this;
 
                 // if (theLocation) {
                     return new Promise(function (resolve, reject) {
-                        geocoder.geocode({'address': theLocation.default()}, function (results, status) {
+                        geocoder.geocode({'address': (theLocation ? theLocation : 'canada')}, function (results, status) {
                             console.log(results);
                             if (status === google.maps.GeocoderStatus.OK) {
                                 console.log(results[0].geometry.location.lat(), results[0].geometry.location.lng());
                                 latLong.latitude = results[0].geometry.location.lat();
                                 latLong.longitude = results[0].geometry.location.lng();
+                                var myLatlng = new google.maps.LatLng(latLong.latitude, latLong.longitude);
+                                self.map.panTo(myLatlng);
                             } else {
                                 reject(status);
                             }
@@ -73,13 +71,16 @@
             }
         },
         mounted() {
+            //this.geoDecoding();
             this.showMap();
         },
         created() {
+            //this.showMap();
+            //this.geoDecoding();
             Bus.$on('passLocation', (input) => {
                 this.location = input;
+                this.geoDecoding();
             });
-            this.geoDecoding();
         },
 
 
@@ -92,6 +93,5 @@
         margin: 5vh auto;
         height: 50vh;
         background: fuchsia;
-        content: 'PLEASE BE PATIENT ON A SLOW CONNECTIONS';
     }
 </style>
