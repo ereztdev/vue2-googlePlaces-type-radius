b7f57acf22fa242b8cfb718faa1a65c954d4c7ab
erezt.dev <erezt.dev@gmail.com>
2018-04-25 13:54:21 +0300

    created info window, will parse and populate response

---
+++ src/components/InfoWindow.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="info-window--wrapper">
+        <div id="info-window">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "InfoWindow"
+    }
+</script>
+
+<style scoped>
+    .info-window--wrapper {
+        text-align: center;
+    }
+
+    #info-window {
+        margin: 0 auto;
+        width: 100%;
+        height: 20vh;
+        background: aquamarine;
+    }
+</style>
