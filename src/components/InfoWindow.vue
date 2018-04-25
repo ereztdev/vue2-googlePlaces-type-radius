150785c0001cc1ecd385bf6230ccad2cd351a860
erezt.dev <erezt.dev@gmail.com>
2018-04-25 17:59:15 +0300

    infoWindow is partial

--- src/components/InfoWindow.vue
+++ src/components/InfoWindow.vue
@@ -2,8 +2,14 @@
     <div class="info-window--wrapper">
         <div id="info-window">
             <ul>
-                <li v-for="result in testing">
-                    {{ result }}
+                <li v-for="result in searchResults">
+                    <img v-if="result.photos" :src="result.photos[0].getUrl({ 'maxWidth': 140, 'maxHeight': 100 })" />
+                    <img v-else src="http://via.placeholder.com/140x100" />
+                    <h2>{{result.name}}</h2>
+                    <p v-if="result.opening_hours">Is it open now? <span v-bind:style="result.opening_hours.open_now ? 'color:green' : 'color:red'">{{(result.opening_hours.open_now ? 'Yes :)': 'Nope :(')}}</span></p>
+                    <p v-else>Is it open now? No info on that.</p>
+                    <p class="venue-types">This location is associated with the following venue types: <strong>{{(result.types.filter(type => type !== "establishment" && type !== "point_of_interest").join(", ").replace("_", " "))}}</strong></p>
+
                 </li>
             </ul>
         </div>
@@ -17,27 +23,18 @@
         name: "InfoWindow",
         data() {
             return {
-                testing: ['accounting',
-                    'airport',
-                    'amusement_park',
-                    'aquarium',
-                    'art_gallery',
-                    'atm',
-                    'bakery',
-                    'bank',
-                    'bar',
-                    'beauty_salon',
-                    'bicycle_store',
-                    'book_store',
-                    'bowling_alley',
-                    'bus_station',
-                    'cafe'],
-                searchResults: []
+                searchResults: [],
             }
         },
+        // computed:{
+        //   venueTypes(){
+        //       let theVenueTypes =
+        //   }
+        // },
         methods: {
             parseSearchResults() {
-                console.log('oh hello');
+                let i;
+                console.log(this.searchResults);
                 //console.log(this.searchResults);
                 // let i;
                 // for (i = 0; i < this.searchResults; i++) {
@@ -75,8 +72,21 @@
 
     #info-window ul {
         list-style-type: none;
+        text-align: left;
     }
     #info-window li {
-        text-align: left;
+        display: table;
+
+    }
+    #info-window h2 {
+
+    }
+    #info-window h2, p{
+        display: table-cell;
+        padding-left: 40px;
+        vertical-align: middle;
+    }
+    .venue-types{
+        text-decoration: underline;
     }
 </style>
