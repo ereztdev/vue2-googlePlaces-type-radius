530c3f1aaf80858d170246c057fab3f354e4eadc
erezt.dev <erezt.dev@gmail.com>
2018-04-25 13:53:45 +0300

    google places is in with service producing markers. bug: not able to erase previous markers (look @ erez_markers) feature: cool animation that would fade search out and split screen 33/66 to show only map and infoWindow

--- src/components/GoogleMapsContainer.vue
+++ src/components/GoogleMapsContainer.vue
@@ -35,55 +35,55 @@
                 let latLong = this.latLong;
                 self = this;
 
+                let service = new google.maps.places.PlacesService(this.map);
+                var erez_markers = [];
+
                 return new Promise(function (resolve, reject) {
-                    geocoder.geocode({'address': (theLocation ? theLocation : 'canada')}, function (results, status) {
+                    geocoder.geocode({'address': theLocation}, function (results, status) {
                         if (status === google.maps.GeocoderStatus.OK) {
                             console.log(results[0].geometry.location.lat(), results[0].geometry.location.lng());
                             latLong.latitude = results[0].geometry.location.lat();
                             latLong.longitude = results[0].geometry.location.lng();
-                            let myLatlng = new google.maps.LatLng(latLong.latitude, latLong.longitude);
-                            self.map.panTo(myLatlng);
+                            this.myLatlng = new google.maps.LatLng(latLong.latitude, latLong.longitude);
+                            self.map.panTo(this.myLatlng);
+
+                            let request = {
+                                location: this.myLatlng,
+                                radius: self.radius,
+                                type: self.interest
+                            };
+
+                            service.nearbySearch(request, callback);
+
+                            function callback(results, status) {
+                                console.log("results", results);
+                                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                                    for (var i = 0; i < erez_markers.length; i++) {
+                                        erez_markers[i].setMap(null);
+                                    }
+                                    erez_markers = [];
+                                    for (let i = 0; i < results.length; i++) {
+                                        createMarker(results[i]);
+                                    }
+                                    console.log(results);
+                                    console.log(status);
+                                }
+                            }
+
+                            function createMarker(place) {
+                                let placeLoc = place.geometry.location;
+                                let marker = new google.maps.Marker({
+                                    map: self.map,
+                                    position: place.geometry.location
+                                });
+                                erez_markers.push(marker);
+                            }
+
                         } else {
                             reject(status);
                         }
                     });
                 });
-            },
-            getPlaces() {
-                let map;
-                let service;
-                let infowindow;
-
-                function initialize() {
-                    let pyrmont = new google.maps.LatLng(this.latLong.latitude, this.latLong.longitude);
-                    console.log(pyrmont);
-
-                    map = new google.maps.Map(document.getElementById(this.theMap), {
-                        center: pyrmont,
-                        zoom: this.zoom
-                    });
-
-                    let request = {
-                        location: pyrmont,
-                        radius: this.radius,
-                        type: [this.interest]
-                    };
-
-                    service = new google.maps.places.PlacesService(map);
-                    service.nearbySearch(request, callback);
-
-                    function callback(results, status) {
-                        if (status === google.maps.places.PlacesServiceStatus.OK) {
-                            for (let i = 0; i < results.length; i++) {
-                                let place = results[i];
-                                createMarker(results[i]);
-                            }
-                            console.log(results);
-                            console.log(status);
-                        }
-                    }
-                }
-
             }
         },
         mounted() {
@@ -104,19 +104,18 @@
                 Bus.$on('passRadius', (input) => {
                     this.radius = input;
                 });
-                this.getPlaces();
+                // this.getPlaces();
             }
         },
-
-
     }
 </script>
 
 <style scoped>
     .map-container {
-        width: 80vw;
-        margin: 5vh auto;
-        height: 50vh;
+        /*height: calc((100vh/3)*2);*/
+        height: 46vh;
+        margin: 0 auto;
+        width: 100%;
         background: fuchsia;
     }
 </style>
